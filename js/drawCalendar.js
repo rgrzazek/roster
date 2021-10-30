680642689ddb296fc4265d3c26bfe426f10fd57f
rgrzazek <91814115+rgrzazek@users.noreply.github.com>
2021-10-30 11:59:21 +1000

    Add files via upload

--- js/drawCalendar.js
+++ js/drawCalendar.js
@@ -9,15 +9,36 @@ const APR = 3;  const MAY = 4;  const JUN = 5;
 const JUL = 6;  const AUG = 7;  const SEP = 8;
 const OCT = 9;  const NOV = 10; const DEC = 11;
 
-// Hard coded roster dates. Can be any date that coincides
-// with the roster, these dates are not special.
+const DAY = 0;
+const NIGHT = 1;
+const BREAK = 2;
+
+/*
+ * Hard coded values, set up any roster by changing the following 
+ * variables. Note that roster is calculated forward and back, any
+ * valid dates for the start of a roster cycle will work.
+ * 
+ * - roster [ [Type (Day/night/break), length of block], ... ]
+ * - allStartDates & crewLabels (same length)
+ * 
+ * The roster cycle can be any length
+ * "allStartDates" corresponds to "crewLabels"
+ */
+const roster = [ [DAY, 5], [NIGHT, 5], [BREAK, 10] ];
+let rosterLength=0;
+roster.forEach(block => {
+  rosterLength += block[1];
+});
+
+// Hard coded roster dates. Code will calculate forward or back
 let allStartDates = [
-  new Date(2021, OCT, 11),
-  new Date(2021, OCT, 16),
-  new Date(2021, OCT, 21),
-  new Date(2021, OCT, 26)];
-let crew = ["A crew", "B crew", "C crew", "D crew"];
+  new Date(2021, OCT, 12),
+  new Date(2021, OCT, 17),
+  new Date(2021, OCT, 22),
+  new Date(2021, OCT, 27)];
+let crewLabels = ["A crew", "B crew", "C crew", "D crew"];
 let rosterIndex = 0;
+/******************** End customising code ********************/
 
 function changeRoster() {
   rosterIndex = (rosterIndex+1)%allStartDates.length;
@@ -38,16 +59,18 @@ $(window).on("load", function() {
 });
 
 function newCalendar() {
+  // year and month already refer to calendar date
+  startOfMonth = new Date(year, month);
+  startOfNext = new Date(year, month+1);
+
   $("#calendar #grid").html(calendarHTML());
   drawDays();
   applyRoster();
 }
 
 function applyRoster() {
-  roster = [5, 5, 10];
-  rosterLength = 20;
 
-  $("#calendar #crew").html(crew[rosterIndex]);
+  $("#calendar #crew").html(crewLabels[rosterIndex]);
 
   let ptr = allStartDates[rosterIndex];
   let next = new Date(ptr.getFullYear(), ptr.getMonth(), ptr.getDate()+rosterLength);
@@ -56,90 +79,43 @@ function applyRoster() {
   while (ptr>startOfMonth) {
     ptr = new Date(ptr.getFullYear(), ptr.getMonth(), ptr.getDate()-rosterLength);
   }
-  // walk forward: start a period that covers the first of this month
+  // walk forward: start at a period that covers the first of this month
   while (next<startOfMonth) {
     ptr = next;
     next = new Date(ptr.getFullYear(), ptr.getMonth(), ptr.getDate()+rosterLength);
   }
 
-  // Calculate whole cycles, repeat until cycle passes end of month
-  // rely on fact that adding days will auto-change month
+  // Calculate roster cycles, repeat until cycle passes end of month
   while (ptr<startOfNext) {
-    // 5 days
-    for (let day=0; day<5; day++) {
-      if (ptr.getMonth()===month) {
-        getDateRef(ptr.getDate()).addClass("dayshift");
+    roster.forEach(block => {
+      for (let day=0; day<block[1]; day++) {
+        if (ptr.getMonth()===month) {
+          switch (block[0]) {
+            case DAY: getDateRef(ptr.getDate()).addClass("dayshift");
+                      break;
+            case NIGHT: getDateRef(ptr.getDate()).addClass("nightshift");
+                        break;
+          }
+        }
+        ptr.setDate(ptr.getDate()+1);
       }
-      ptr.setDate(ptr.getDate()+1);
-    }
-    // 5 nights
-    for (let day=0; day<5; day++) {
-      if (ptr.getMonth()===month) {
-        getDateRef(ptr.getDate()).addClass("nightshift");
-      }
-      ptr.setDate(ptr.getDate()+1);
-    }
-    // 10 days off
-    ptr.setDate(ptr.getDate()+10);
+    });
   }
-
 }
 
-
+// find dom object for the cell <td> of the table
 function getDateRef(date) {
   date--; // look for 0-based array index
   let dayOfFirst = (new Date(year, month)).getDay();;
-//  let daysInMonth = 32 - new Date(year, month, 32).getDate();
 
-  let week = Math.trunc((date+dayOfFirst)/7);
-  let day = (date+dayOfFirst)%7;
+  let week = Math.trunc((date+dayOfFirst)/7); // div
+  let day = (date+dayOfFirst)%7; // mod
   return $("#week"+week+" #"+daysOfWeek[day]);
-
-}
-
-function drawDays() {
-  $("#calendar #monthAndYear").html(months[month]+" "+year);
-
-  let dayOfFirst = (new Date(year, month)).getDay();
-  let daysInMonth = 32 - new Date(year, month, 32).getDate();
-  
-  for (let i=0; i<daysInMonth; i++) {
-    let week = Math.trunc((i+dayOfFirst)/7);
-    let day = (i+dayOfFirst)%7;
-    $("#week"+week+" #"+daysOfWeek[day]).html( i+1 );
-
-  }
-}
-
-function previousMonth() {
-  if (month === JAN) {
-    year--;
-    month = DEC;
-  } else {
-    month--;
-  }
-  startOfMonth = new Date(year, month);
-  startOfNext = new Date(year, month+1);
-  newCalendar();
-}
-
-function nextMonth() {
-  if (month === DEC) {
-    year++;
-    month = JAN;
-  } else {
-    month++;
-  }
-  startOfMonth = new Date(year, month);
-  startOfNext = new Date(year, month+1);
-  newCalendar();
 }
 
 function calendarHTML() {
   let calendar = '';
-
-    // for example, Wednesday the 6th, in the second calendar week:
-    // cells will have row id: "week2" and cell id "Wed" 
+    // all months have 6 rows regardless
     for (let i=0; i<6; i++) {
       calendar += '<tr id="week'+i+'">';
       for (let j=0; j<7; j++) {
@@ -150,4 +126,26 @@ function calendarHTML() {
     return calendar;
 }
 
+function drawDays() {
+  $("#calendar #monthAndYear").html(months[month]+" "+year);
+  let dayOfFirst = (new Date(year, month)).getDay();
+  let daysInMonth = 32 - new Date(year, month, 32).getDate();
+  
+  for (let i=0; i<daysInMonth; i++) {
+    let week = Math.trunc((i+dayOfFirst)/7);
+    let day = (i+dayOfFirst)%7;
+    $("#week"+week+" #"+daysOfWeek[day]).html( i+1 );
+  }
+}
 
+function previousMonth() {
+  if (month === JAN) {year--;}
+  month = (month===JAN)?DEC:month-1;
+  newCalendar();
+}
+
+function nextMonth() {
+  if (month === DEC) {year++;}
+  month = (month===DEC)?JAN:month+1;
+  newCalendar();
+}
